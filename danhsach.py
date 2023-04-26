9b06a255c6f2530c11992c2556fe3bfabeff6bcb
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-26 23:53:31 +0700

    Update danhsach.py

--- danhsach.py
+++ danhsach.py
@@ -1,4 +1,7 @@
 import pandas as pd
 import streamlit as st
 df = pd.read_csv("py4ai-score.csv", low_memory=False)
+st.text('This is a text')
+st.code('x = 5\nprint(x)')
+st.latex('x^2 + \sqrt{y} = \pi')
 st.write(df) 
