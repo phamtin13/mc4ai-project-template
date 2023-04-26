340fbf06a5438d668bd9d89f21f02cd265b25d9f
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-27 00:01:45 +0700

    Update danhsach.py

--- danhsach.py
+++ danhsach.py
@@ -3,9 +3,14 @@ import streamlit as st
 def danhsach():
   df = pd.read_csv("py4ai-score.csv", low_memory=False)
   COLS = df.columns.values.tolist().copy()
-  check = st.checkbox('Boy')
-  radio = st.radio('Radio', ('Option 1', 'Option 2', 'Option 3'), horizontal=True)
-  if st.button('OK'):
-    st.write('Check box:', check)
-    st.write('Radio:', radio, type(radio))
+  option = st.selectbox('Selectbox', ('select 1', 'select 2', 'select 3'))
+  options = st.multiselect('Multiselect', ['Green', 'Yellow', 'Red', 'Blue'], ['Yellow', 'Red'])
+  slider = st.slider('Slider', 0, 130, 25)
+  day = st.select_slider('Select slider', options=['Monday', 'Tuesday', 'Wednesday', 'Friday', 'Saturday', 'Sunday'], value=['Monday', 'Wednesday'])
+
+  if st.button('Click'):
+    st.write('select box:', option)
+    st.write('multiselect:', options, type(options))
+    st.write('slider:', slider)
+    st.write('select slider:', day)
 danhsach()
