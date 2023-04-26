80f74f89cc048eee5e17a70095d61025429f07f7
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-27 00:17:08 +0700

    Update danhsach.py

--- danhsach.py
+++ danhsach.py
@@ -2,6 +2,13 @@ import pandas as pd
 import streamlit as st
 def danhsach():
   df = pd.read_csv("py4ai-score.csv", low_memory=False)
-  COLS = df.columns.values.tolist().copy()
-  st.write(COLS[0])
+  dfmid = df.copy()
+  COLS = dfmid.columns.values.tolist().copy()
+  def gen(row):
+    if row == 'M':
+      return 'Nam'
+    else:
+      return 'Nữ'
+  dfmid['Gen'] = dfmid.apply(grade, axis=1)
+  st.write(dfmid)
 danhsach()
