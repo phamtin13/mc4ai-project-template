db59d1a29eda2c78c0867b59f7fc83e3680bb1ad
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-27 00:07:51 +0700

    Update danhsach.py

--- danhsach.py
+++ danhsach.py
@@ -3,5 +3,5 @@ import streamlit as st
 def danhsach():
   df = pd.read_csv("py4ai-score.csv", low_memory=False)
   COLS = df.columns.values.tolist().copy()
-  st.write(COLS)
+  st.write(COLS[0])
 danhsach()
