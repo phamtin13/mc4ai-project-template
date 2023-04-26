d7bd839b7902a4b1a773e9fecfb8f91562f2624f
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-26 23:14:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 import streamlit as st
-st.title('BẢNG ĐIỂM LỚP PY4AI 09/2022')
-tab1, tab2, tab3, tab4 = st.tabs(['Danh sách','Biểu đồ','Phân nhóm','Phân loại'])
+def main():
+  st.title('BẢNG ĐIỂM LỚP PY4AI 09/2022')
+  tab1, tab2, tab3, tab4 = st.tabs(['Danh sách','Biểu đồ','Phân nhóm','Phân loại'])
+
+main()
