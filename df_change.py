4a5588d38130c74e6b8845862672c3db7f4625d6
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-27 16:05:58 +0000

    Update df_change.py

--- df_change.py
+++ df_change.py
@@ -13,6 +13,6 @@ def df_change():
       return 'Nữ'
   dfmid['Gen'] = dfmid.apply(gen, axis=1)
   st.write(dfmid)
-  st.write(dfmid[COLS[1]][0])
+  st.write(np.unique(dfmid[COLS[1]]))
   
 df_change()
