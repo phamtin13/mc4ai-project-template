f906f91e2ed62344abd2b9294c8ccf4dc7098d11
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-27 14:00:57 +0700

    Update danhsach.py

--- danhsach.py
+++ danhsach.py
@@ -10,5 +10,23 @@ def danhsach():
     else:
       return 'Nữ'
   dfmid['Gen'] = dfmid.apply(gen, axis=1)
-  st.write(dfmid)
+  st.title("Todo list app")
+
+# 1. Create a variable to store todos.
+  if not 'todolist' in st.session_state:
+    st.session_state.todolist = []
+
+# 2. Prompt the user in the form
+  with st.form(key='form'):
+    todo = st.text_input(label='Enter todo description')
+    is_submit = st.form_submit_button('submit')
+
+# 3. Store todo in todolist when submit button is hit.
+  if is_submit:
+    st.session_state.todolist.append(todo)
+    
+# 4. Display the contents of todolist
+  with st.expander(label='List of todos', expanded=True):
+    for i, todo_text in enumerate(st.session_state.todolist):
+      st.checkbox(label=f'{todo_text}', key=i)
 danhsach()
