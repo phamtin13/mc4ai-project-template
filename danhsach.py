edc47e857ddf1f96a51b66fb451b4bcd4efcb7c7
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-27 09:21:03 +0000

    Update danhsach.py

--- danhsach.py
+++ danhsach.py
@@ -6,6 +6,7 @@ def danhsach():
   df = pd.read_csv("py4ai-score.csv", low_memory=False)
   COLS = df.columns.values.tolist().copy()
   col1, col2, col3, col4 = st.columns(4)
+  st.title('Hello')
   
   with col1:
     st.write('Giới tính')  
