d5365c8b2873316291c560b1397390f6c53edf70
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-27 09:18:33 +0000

    Update danhsach.py

--- danhsach.py
+++ danhsach.py
@@ -1,2 +1,11 @@
 import streamlit as st
-st.write('Hello')
+import pandas as pd
+import numpy as np
+
+def danhsach():
+  df = pd.read_csv("py4ai-score.csv", low_memory=False)
+  COLS = df.columns.values.tolist().copy()
+  col1, col2, col3, col4 = st.columns(4)
+  
+  with col1:
+    st.write('Giới tính')  
