54dd193dbd87c6aa0c1dadb9a2de0c9d6eff4b48
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-27 09:30:06 +0000

    Update danhsach.py

--- danhsach.py
+++ danhsach.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 
 def danhsach():
-  #df = pd.read_csv("py4ai-score.csv", low_memory=False)
-  #COLS = df.columns.values.tolist().copy()
+  df = pd.read_csv("py4ai-score.csv", low_memory=False)
+  COLS = df.columns.values.tolist().copy()
   col1, col2, col3, col4 = st.columns(4)
   st.title('Hello')
   
