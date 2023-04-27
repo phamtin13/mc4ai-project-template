4be72076788ccef8aff84e5859863f33d4f24da8
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-27 17:23:54 +0000

    Update danhsach.py

--- danhsach.py
+++ danhsach.py
@@ -5,14 +5,14 @@ from df_change import df_change
 
 def danhsach():
   df = pd.read_csv("py4ai-score.csv", low_memory=False)
-  COLS = df.columns.values.tolist().copy()
   col1, col2, col3, col4 = st.columns(4)
   dfmid = df_change()
+  COLS_mid = dfmid.columns.values.tolist().copy()
   with col1:
     st.write('Giới tính')
  
   st.write(dfmid)
-  df.drop(columns=COLS[17:], inplace=True)
+  dfmid.drop(columns=COLS_mid[17:], inplace=True)
   st.write(dfmid)
     
 danhsach()
