6f0d5908a1b1c6ca468d59ebbe67b79f88bbd9a9
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-28 09:01:06 +0000

    Update danhsach.py

--- danhsach.py
+++ danhsach.py
@@ -4,7 +4,6 @@ import numpy as np
 from df_change import df_change
 
 def danhsach():
-  df = pd.read_csv("py4ai-score.csv", low_memory=False)
   col1, col2, col3, col4 = st.columns(4)
   dfmid = df_change()
   COLS_mid = dfmid.columns.values.tolist().copy()
