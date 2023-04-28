9cbc81b7f8b25c97436411fb32168ff132515859
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-28 08:12:20 +0000

    Update test.py

--- test.py
+++ test.py
@@ -5,11 +5,9 @@ from df_change import df_change
 dfmid = df_change()
 check = np.array([st.checkbox(i) for i in np.unique(dfmid['Subject'])])
 A = np.stack((check,np.unique(dfmid['Subject']))).T
-st.write(A)
 needrop = []
 for i in A:
   if i[0] is False:
     needrop += np.where(dfmid['Subject']==i[1])[0].tolist()
-print(len(needrop))
 dfmid.drop(needrop, inplace=True)
-st.write(needrop)
+st.write(dfmid)
