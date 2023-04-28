b88b8c008798fd9fb1e98140530b28faedf068b5
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-28 05:35:05 +0000

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,8 @@ from df_change import df_change
 dfmid = df_change()
 check = np.array([st.checkbox(i) for i in np.unique(dfmid['Subject'])])
 A = np.stack((check,np.unique(dfmid['Subject'])).T
-for i in A:
-  if i[0]:
-    dfmid = dfmid.loc[dfmid['Subject']==i[1]]
-st.write(dfmid)
+st.write(A)
+#for i in A:
+  #if i[0]:
+    #dfmid = dfmid.loc[dfmid['Subject']==i[1]]
+#st.write(dfmid)
