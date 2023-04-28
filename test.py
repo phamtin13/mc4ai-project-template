95805f60ae135bceee384fdc14d488c2c53c01f2
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-28 06:09:26 +0000

    Update test.py

--- test.py
+++ test.py
@@ -8,5 +8,5 @@ A = np.stack((check,np.unique(dfmid['Subject']))).T
 st.write(A)
 for i in A:
   if i[0] is False:
-    dfmid.drop(np.where(dfmid['Subject']==i[1]), inplace=True)
+    dfmid.drop(np.where(dfmid['Subject']==i[1])[0], inplace=True)
 st.write(dfmid)
