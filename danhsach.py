f700c33e75e66ea53c458b8b86816ec990701db3
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-28 08:31:31 +0000

    Update danhsach.py

--- danhsach.py
+++ danhsach.py
@@ -8,10 +8,13 @@ def danhsach():
   col1, col2, col3, col4 = st.columns(4)
   dfmid = df_change()
   COLS_mid = dfmid.columns.values.tolist().copy()
+  needrop = []
+  
   with col1:
     st.write('Giới tính')
- 
-  st.write(dfmid)
+    check_gender = np.array([st.checkbox(i) for i in np.unique(dfmid['Gen'])])
+    A_gender = np.stack((check_gender,np.unique(dfmid['Gen']),np.array(['Gen']*len(np.unique(dfmid['Gen'])))))
+    st.write(A_gender)
   dfmid.drop(columns=COLS_mid[17:], inplace=True)
   st.write(dfmid)
     
