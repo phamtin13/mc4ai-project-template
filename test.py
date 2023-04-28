eeca1b625ac537a7e525d6bde415fd1dbd83cffe
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-28 05:33:10 +0000

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,10 @@ import streamlit as st
 import numpy as np
 from df_change import df_change
 
-A = [st.checkbox(i) for i in np.unique(dfmid['Subject'])]
+dfmid = df_change()
+check = np.array([st.checkbox(i) for i in np.unique(dfmid['Subject'])])
+A = np.stack((check,np.unique(dfmid['Subject'])).T
+for i in A:
+  if i[0]:
+    dfmid = dfmid.loc[dfmid['Subject']==i[1]]
+st.write(dfmid)
