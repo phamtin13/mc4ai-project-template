594aea5d302174b21eff53ac6e87f053b0f4e33d
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-28 16:03:22 +0000

    Update danhsach.py

--- danhsach.py
+++ danhsach.py
@@ -56,13 +56,13 @@ def danhsach():
   A_sub = np.stack((check_sub,unisub,['Subject']*len(unisub))).T
   
   A = np.concatenate((A_gender, A_grade, A_room, A_day, A_sub))
-  
+  st.write(A)
   needrop = []
   for i in A:
     if i[0] == False:
       needrop += np.where(dfmid[i[2]]==i[1])[0].tolist()
   dfmid.drop(np.unique(needrop), inplace=True)
   dfmid.drop(columns=COLS_mid[17:], inplace=True)
-  st.write(dfmid)
+  #st.write(dfmid)
     
 danhsach()
