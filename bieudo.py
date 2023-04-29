80d5a422e58f9139311170ee629792be75712701
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-29 05:03:02 +0000

    Create bieudo.py

---
+++ bieudo.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import numpy as np
+import plotly.express as px
+from df_change import df_change
+
+dfmid = df_change()
+
+tab1, tab2 = st.tabs(["Số lượng học sinh", "Điểm"])
+
+with tab1:
+  def pie(df,a):
+    return px.pie(df, names=a)
+  
+  nhanxet = ['Gen','* Tín * hưng']
+  st.write(nhanxet[1])
