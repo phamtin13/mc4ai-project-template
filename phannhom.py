a9a396a6b6691512c398579ff11723d0c7e178ee
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-29 15:15:55 +0000

    Update phannhom.py

--- phannhom.py
+++ phannhom.py
@@ -9,9 +9,11 @@ def phannhom():
   dfmid.rename(columns={'S6' : 'Midterm Exam','S10' : 'Final Exam'}, inplace=True)
   slider = st.slider('Số nhóm:', min_value = 2, max_value = 5, step = 1)
   kmeans = KMeans(n_clusters=slider, n_init='auto')
-  X = np.stack((dfmid['Homework'],dfmid['Midterm Exam'],dfmid['Final Exam'])).T
+  #X = np.stack((dfmid['Homework'],dfmid['Midterm Exam'],dfmid['Final Exam'])).T
+  X = np.stack((dfmid['Homework'],dfmid['Midterm Exam'],dfmid['GPA'])).T
   kmeans.fit(X)
-  st.write(px.scatter_3d(dfmid, x = 'Homework', y = 'Midterm Exam', z = 'Final Exam', color = kmeans.labels_))
+  #st.write(px.scatter_3d(dfmid, x = 'Homework', y = 'Midterm Exam', z = 'Final Exam', color = kmeans.labels_))
+  st.write(px.scatter_3d(dfmid, x = 'Homework', y = 'Midterm Exam', z = 'GPA', color = kmeans.labels_))
   
   labels = np.array(['Nhóm '+str(i+1) for i in np.unique(kmeans.labels_)])
   kmeans.labels_ = np.array([str(i) for i in kmeans.labels_])
