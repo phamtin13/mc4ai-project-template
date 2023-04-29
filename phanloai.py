aa332c2a6f6a4feb3a87211952726846df0a526c
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-29 15:38:28 +0000

    Update phanloai.py

--- phanloai.py
+++ phanloai.py
@@ -8,9 +8,10 @@ def phanloai():
   dfmid = df_change()
   dfmid.rename(columns={'S6' : 'Midterm Exam','S10' : 'Final Exam'}, inplace=True)
   option = st.selectbox('Số đặc trưng', ('2 đặc trưng', '3 đặc trưng'))
+  
   if option == '3 đặc trưng':
-    kmeans = KMeans(n_clusters=2, n_init='auto')
     X = np.stack((dfmid['Homework'],dfmid['Midterm Exam'],dfmid['Final Exam'])).T
+    kmeans = KMeans(n_clusters=2, n_init='auto')
     kmeans.fit(X)
     datas = np.linspace(0, 10, 100)
     xx, yy = np.meshgrid(datas, datas)
