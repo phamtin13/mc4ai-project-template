e6966f8d930974e74818bd02240641af65e6f724
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-29 08:16:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 from danhsach import danhsach
+from bieudo import bieudo
+
 def main():
   st.title('BẢNG ĐIỂM LỚP PY4AI 09/2022')
   tab1, tab2, tab3, tab4 = st.tabs(['Danh sách','Biểu đồ','Phân nhóm','Phân loại'])
   
   with tab1:
     danhsach()
-    
+  
+  with tab2:
+    bieudo()
 main()
