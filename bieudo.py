5fe29cff68ad638d4be884fb4b166f940d8afa8b
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-29 06:48:53 +0000

    Update bieudo.py

--- bieudo.py
+++ bieudo.py
@@ -4,16 +4,21 @@ import plotly.express as px
 from df_change import df_change
 
 dfmid = df_change()
+COLS = dfmid.columns.values.tolist().copy()
+COLS.remove('BONUS')
 
 tab1, tab2 = st.tabs(["Số lượng học sinh", "Điểm"])
 
 with tab1:
-  nhanxet = np.array([['Giới tính','Gen','Nhìn chung học sinh Nam hứng thú với AI hơn học sinh Nữ.'],
+  nhanxet1 = np.array([['Giới tính','Gen','Nhìn chung học sinh Nam hứng thú với AI hơn học sinh Nữ.'],
              ['Môn học','Subject','Khối chuyên Toán quan tâm đến AI nhiều nhất và khối chuyên Trung - Nhật ít quan tâm nhất.'],
              ['Lớp Python','PYTHON-CLASS','Số học sinh ở 2 buổi và 2 phòng học gần bằng nhau, nên giờ học là hợp lý, đáp ứng được nhu cầu của tất cả học sinh.'],
              ['Khối lớp','Grade','Số lượng học sinh lớp 10 tham gia khoá PY4AI là nhiều nhất và số lượng học sinh lớp 12 là ít nhất.']])
   
-  option = st.radio('Phân tích biểu đồ hình tròn theo:', nhanxet[:,0], horizontal=True)
-  st.write(px.pie(dfmid, names = nhanxet[:,1][nhanxet[:,0]==option][0]))
-  st.success('Kết luận: '+nhanxet[:,2][nhanxet[:,0]==option][0])
+  pies = st.radio('Phân tích biểu đồ số lượng học sinh theo:', nhanxet[:,0], horizontal=True)
+  st.write(px.pie(dfmid, names = nhanxet[:,1][nhanxet[:,0]==pies][0]))
+  st.success('Kết luận: '+nhanxet[:,2][nhanxet[:,0]==pies][0])
   
+with tab2:
+  types = st.radio('Phân tích điểm theo dạng:', ('Biểu đồ cột (histogram)','Biểu đồ hộp (box)'), horizontal=True)
+  option = st.radio('Điểm từng session:', COLS[4:15], horizontal=True)
