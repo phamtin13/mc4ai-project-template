0dfd5bd2121126f6a2b6c77a26876e0ca1c5b53e
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-29 12:37:42 +0000

    Update phannhom.py

--- phannhom.py
+++ phannhom.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import numpy as np
+from df_change import df_change
 from sklearn.cluster import KMeans
 
 def phannhom():
+  dfmid = df_change()
   slider = st.slider('Số nhóm:', min_value = 2, max_value = 5, step = 1)
   kmeans = KMeans(n_clusters=slider, n_init='auto')
+  st.write(dfmid)
 
 phannhom()
