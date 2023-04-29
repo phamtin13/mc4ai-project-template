e598c0138c80c6fd65daec2ea78c3ede19e851ff
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-29 16:55:23 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ import numpy as np
 from df_change import df_change
 
 
-ts = ['Taylor Swift', 'Kanye West', 'Justin Bieber', 'Selena Gomez']
+ts = ['Taylor Swift', 'Kanye West', 'Justin Bieber', 'Selena Gomez', 'Kim Kardashian']
 options = st.multiselect('Your favorite celebrity', ts)
 st.write('Wow, you chose:',len(options))
-
-    
+st.write(options)
