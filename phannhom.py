77b184de2421a9177ff502991500f933072c8a7f
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-29 12:30:28 +0000

    Update phannhom.py

--- phannhom.py
+++ phannhom.py
@@ -4,5 +4,6 @@ from sklearn.cluster import KMeans
 
 def phannhom():
   slider = st.slider('Số nhóm:', min_value = 2, max_value = 5, step = 1)
+  kmeans = KMeans(n_clusters=slider, n_init='auto')
 
 phannhom()
