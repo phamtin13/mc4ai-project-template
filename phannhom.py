b7e6d734fb7524f3c437878377936a25b46c2455
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-29 13:07:04 +0000

    Update phannhom.py

--- phannhom.py
+++ phannhom.py
@@ -11,6 +11,8 @@ def phannhom():
   kmeans = KMeans(n_clusters=slider, n_init='auto')
   X = np.stack((dfmid['Homework'],dfmid['Midterm Exam'],dfmid['Final Exam'])).T
   kmeans.fit(X)
-  st.write(px.scatter_3d(dfmid, x = 'Homework', y = 'Midterm Exam', z = 'Final Exam', color = kmeans.labels_))
+  labels = kmeans.labels_
+  st.write(px.scatter_3d(dfmid, x = 'Homework', y = 'Midterm Exam', z = 'Final Exam', color = labels))
+  st.write(labels)
 
 phannhom()
