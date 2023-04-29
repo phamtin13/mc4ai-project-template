48aae69106b7966cf854d3a2ae9bef6b7e171fd9
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-29 14:58:44 +0000

    Update phannhom.py

--- phannhom.py
+++ phannhom.py
@@ -21,7 +21,7 @@ def phannhom():
     df_new = df_need[['NAME', 'CLASS', 'Homework', 'Midterm Exam', 'Final Exam', 'GPA']]
   
     new = df_new['GPA']
-    st.subheader(j+' :')
+    st.subheader(j+':')
     st.write('GPA cao nhất',new.max(),', thấp nhất',new.min(),', trung bình',np.round(new.mean(),1))
     st.write(df_new)
   
