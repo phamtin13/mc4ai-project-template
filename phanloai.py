2176073e69719a9c29e3998213111cc5bb2040de
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-04-29 15:33:21 +0000

    Update phanloai.py

--- phanloai.py
+++ phanloai.py
@@ -2,11 +2,21 @@ import streamlit as st
 import numpy as np
 from df_change import df_change
 from sklearn.cluster import KMeans
+import plotly.graph_objects as go
 
 def phanloai():
   dfmid = df_change()
+  dfmid.rename(columns={'S6' : 'Midterm Exam','S10' : 'Final Exam'}, inplace=True)
   option = st.selectbox('Số đặc trưng', ('2 đặc trưng', '3 đặc trưng'))
-  if option == '2 đặc trưng':
-    pass
+  if option == '3 đặc trưng':
+    kmeans = KMeans(n_clusters=2, n_init='auto')
+    X = np.stack((dfmid['Homework'],dfmid['Midterm Exam'],dfmid['Final Exam'])).T
+    kmeans.fit(X)
+    datas = np.linspace(0, 10, 100)
+    xx, yy = np.meshgrid(datas, datas)
+    xy = np.c_[xx.ravel(), yy.ravel()]
+    z = kmeans.predict(xy)
+    st.write(fig = go.Figure(data=[go.Scatter3d(x=X[:,0], y=X[:,1], z=X[:,2], mode='markers'),
+                      go.Surface(x=datas, y=datas, z=z)]))
   
 phanloai()
