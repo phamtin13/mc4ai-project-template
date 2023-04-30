f65898b79d26f652ba68d1b8391c9f852c651a96
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-04-30 05:28:51 +0000

    Update nhanxet.py

--- nhanxet.py
+++ nhanxet.py
@@ -2,7 +2,28 @@ import streamlit as st
 import numpy as np
 from df_change import df_change
 
-def nhanxet():
-  pass
+def nhanxet(option):
+  dfmid = df_change()
+  COLS = dfmid.columns.values.tolist().copy()
+  if types == 'Biểu đồ cột (histogram)':
+    A = []
+    B = []
+    for i in np.unique(dfmid['Gen']):
+      for j in np.unique(dfmid['PYTHON-CLASS']):
+        A.append([dfmid[option][(dfmid['Gen']==i)&(dfmid['PYTHON-CLASS']==j)].sum(),i,j])
+      A = np.array(A)
+      gen = A[A[:,1]==i]
+      a = gen[:,0].astype(float)
+      B.append(gen[np.argmax(a)])
+      B.append(gen[np.argmin(a)])
+      A = A.tolist()
+    B = np.array(B)
+
+    nhanxet = []
+    for i in np.unique(B[:,1]):
+      B1 = B[B[:,1]==i]
+      nhanxet.append('Đối với học sinh '+i+': Lớp '+B1[:,2][0]+' có điểm tổng ít nhất và lớp '+B1[:,2][1]+' có điểm tổng ít nhất.')
+      
+  return nhanxet
 
 nhanxet()
