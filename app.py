4a5cb316d1d94c7d6669e124e974850305f535b9
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-04-30 02:45:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from phannhom import phannhom
 
 def main():
   st.title('BẢNG ĐIỂM LỚP PY4AI 09/2022')
-  tab1, tab2, tab3, tab4 = st.tabs(['Danh sách','Biểu đồ','Phân nhóm','Phân loại'])
+  tab1, tab2, tab3, tab4 = st.tabs(['Danh sách','Biểu đồ','Phân nhóm','Phân loại','Hi'])
   
   with tab1:
     danhsach()
