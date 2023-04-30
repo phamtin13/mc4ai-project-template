0b53c5b15073b9ad1f6196f872b412729157de51
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-04-30 02:21:25 +0000

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import numpy as np
-from df_change import df_change
-
-
-ts = ['Taylor Swift', 'Kanye West', 'Justin Bieber', 'Selena Gomez', 'Kim Kardashian']
-options = st.multiselect('Your favorite celebrity', ts)
-st.write('Wow, you chose:',len(options))
-st.write(options)
