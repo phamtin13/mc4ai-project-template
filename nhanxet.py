df5060aef6935e2e23e9d8ec424a4763e54562ca
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-04-30 06:33:23 +0000

    Update nhanxet.py

--- nhanxet.py
+++ nhanxet.py
@@ -12,19 +12,19 @@ def nhanxet(option,types):
       for j in np.unique(dfmid['PYTHON-CLASS']):
         A.append([dfmid[option][(dfmid['Gen']==i)&(dfmid['PYTHON-CLASS']==j)].sum(),i,j])
       A = np.array(A)
-      gen = A[A[:,1]==i]
-      a = gen[:,0].astype(float)
-      B.append(gen[np.argmax(a)])
-      B.append(gen[np.argmin(a)])
+      a1 = A[A[:,1]==i]
+      a = a1[:,0].astype(float)
+      B.append(a1[np.argmax(a)])
+      B.append(a1[np.argmin(a)])
       A = A.tolist()
     B = np.array(B)
-    c = B[:,0].astype(float)
+    b = B[:,0].astype(float)
 
     nhanxet = []
     for i in np.unique(B[:,1]):
       B1 = B[B[:,1]==i]
-      c1 = c[B[:,1]==i]
-      nhanxet.append('Đối với học sinh '+i+': Lớp '+B1[:,2][0]+' có điểm tổng cao nhất ('+str(np.round(c1[0],1))+' điểm) và lớp '+B1[:,2][1]+' có điểm tổng thấp nhất ('+str(np.round(c1[1],1))+' điểm).')
+      b1 = b[B[:,1]==i]
+      nhanxet.append('Đối với học sinh '+i+': Lớp '+B1[:,2][0]+' có điểm tổng cao nhất ('+str(np.round(b1[0],1))+' điểm) và lớp '+B1[:,2][1]+' có điểm tổng thấp nhất ('+str(np.round(b1[1],1))+' điểm).')
     
     C = []
     D = []
