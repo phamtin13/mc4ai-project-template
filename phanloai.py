72d4761464f6f84875565c17d934030af1ba655c
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-05-01 04:55:11 +0000

    Update phanloai.py

--- phanloai.py
+++ phanloai.py
@@ -18,8 +18,6 @@ def phanloai():
       check.append(str(cols[j].checkbox(options[5*i+j],key=str(options[5*i+j])+' key')))
   A = np.stack((check,options)).T
   st.write(A)
-  st.write(A[A[:,0]=='True'])
-  st.write(len(A[A[:,0]=='True']))
   
   if len(A[A[:,0]=='True']) == 0:
     st.info('Hãy chọn 2 hoặc 3 đặc trưng mà bạn muốn.')
