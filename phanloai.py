32fc30a899f9025e49763c3cdb8990a350bd8de2
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-05-01 16:32:15 +0000

    Update phanloai.py

--- phanloai.py
+++ phanloai.py
@@ -65,7 +65,7 @@ def phanloai():
     data.append(go.Surface(x=x, y=y, z=z))
     fig = go.Figure(data=data)
     fig.update_layout(showlegend=True,scene = dict(xaxis = dict(title=choice[0]),yaxis = dict(title=choice[1]),zaxis = dict(title=choice[2])))
-    st.pyplot(fig)
+    st.plotly_chart(fig)
     st.success('Score: '+str(np.round(model.score(X, y)*100,1))+'%')
   
 phanloai()
