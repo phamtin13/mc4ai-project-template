4343db18138a52588c4589dbc8a8ded3a04cfcd7
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-05-01 03:57:40 +0000

    Update phanloai.py

--- phanloai.py
+++ phanloai.py
@@ -9,16 +9,16 @@ def phanloai():
   dfmid.rename(columns={'S6' : 'Midterm Exam','S10' : 'Final Exam'}, inplace=True)
   option = st.selectbox('Số đặc trưng', ('2 đặc trưng', '3 đặc trưng'))
   
-  if option == '3 đặc trưng':
-    X = np.stack((dfmid['Homework'],dfmid['Midterm Exam'],dfmid['Final Exam'])).T
-    kmeans = KMeans(n_clusters=2, n_init='auto')
-    kmeans.fit(X)
-    datas = np.linspace(0, 10, 100)
-    xx, yy = np.meshgrid(datas, datas)
-    xy = np.c_[xx.ravel(), yy.ravel()]
-    z = kmeans.predict(xy)
-    z = z.reshape(xx.shape)
-    st.write(fig = go.Figure(data=[go.Scatter3d(x=X[:,0], y=X[:,1], z=X[:,2], mode='markers'),
-                      go.Surface(x=datas, y=datas, z=z)]))
+  #if option == '3 đặc trưng':
+    #X = np.stack((dfmid['Homework'],dfmid['Midterm Exam'],dfmid['Final Exam'])).T
+    #kmeans = KMeans(n_clusters=2, n_init='auto')
+    #kmeans.fit(X)
+    #datas = np.linspace(0, 10, 100)
+    #xx, yy = np.meshgrid(datas, datas)
+    #xy = np.c_[xx.ravel(), yy.ravel()]
+    #z = kmeans.predict(xy)
+    #z = z.reshape(xx.shape)
+    #st.write(fig = go.Figure(data=[go.Scatter3d(x=X[:,0], y=X[:,1], z=X[:,2], mode='markers'),
+                      #go.Surface(x=datas, y=datas, z=z)]))
   
 phanloai()
