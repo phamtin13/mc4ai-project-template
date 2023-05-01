dc6ccb29a71654ff9276bc9940569512db823f10
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-05-01 12:35:18 +0000

    Update phanloai.py

--- phanloai.py
+++ phanloai.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import matplotlib.pyplot as plt
 from df_change import df_change
 from sklearn.cluster import KMeans
 from sklearn.linear_model import LogisticRegression
@@ -19,7 +20,7 @@ def phanloai():
       check.append(str(cols[j].checkbox(options[5*i+j],key=str(options[5*i+j])+' key')))
   A = np.stack((check,options)).T
   X = dfmid[A[A[:,0]=='True',1]]
-  st.write(X)
+  y = dfmid['Fail or Pass']
   
   if len(A[A[:,0]=='True']) == 0:
     st.info('Hãy chọn 2 hoặc 3 đặc trưng mà bạn muốn.')
@@ -29,6 +30,15 @@ def phanloai():
     
   elif len(A[A[:,0]=='True']) == 2:
     model = LogisticRegression()
+    model.fit(X, y)
+    weights = model.coef_[0]
+    bias = model.intercept_[0]
+    w1, w2 = weights
+    
+    for i in np.unique(dfmid['Fail or Pass']):
+      st.write(plt.scatter(X[y==i,0],X[y==i,1]))
+    plt.legend(np.unique(dfmid['Fail or Pass']))
+    st.write(plt.plot(x,-(w1*x+bias)/w2))
    
   else:
     st.error('Xin lỗi, bạn chỉ được chọn 2 hoặc 3 đặc trưng thôi. Xin hãy chọn lại.')
