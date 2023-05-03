35d745c75c2b7e3940cc3a8d78c448a1d53026d4
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-05-03 16:23:22 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,17 +3,14 @@ import numpy as np
 from df_change import df_change
 dfmid = df_change()
 uniday = np.unique(dfmid['Part of day'])[::-1].tolist()
-uniday_keep = uniday.copy()
 options = st.multiselect('Buổi:', uniday)
-if len(options) == 0 or len(options) == len(uniday_keep):
-  st.write('Giống')
+if len(options) == 0 or len(options) == len(uniday):
   A_day = np.stack((['True']*len(uniday),uniday,['Part of day']*len(uniday))).T
 else:
-  st.write('Khác')
-  #uniday.remove(options[0])
-  A_day = np.stack((['False']*len(uniday),uniday,['Part of day']*len(uniday))).T
+  difday = list(set(uniday) - set(options))
+  A_day = np.stack((['False']*len(difday),difday,['Part of day']*len(difday))).T
 st.write(uniday)
 st.write(len(uniday))
 st.write(options)
 st.write(len(options))
-#st.write(A_day)
+st.write(A_day)
