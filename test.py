813b82b1186c88fe7d91fe6cc3fbec7a08317335
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-05-03 15:50:33 +0000

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import numpy as np
+from df_change import df_change
+dfmid = df_change()
+uniday = np.unique(dfmid['Part of day'])[::-1].tolist()
+options = st.multiselect('Buổi:', uniday)
+if len(options) != 0 and len(options) != len(uniday):
+for i in options:
+  uniday.remove(i)
+  A_day = np.stack((['False']*len(uniday),uniday,['Part of day']*len(uniday))).T
+else:
+  A_day = np.stack((['True']*len(uniday),uniday,['Part of day']*len(uniday))).T
+st.write(len(uniday))
+st.write(len(options))
+st.write(A_day)
