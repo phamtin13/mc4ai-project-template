284b320e072bcbd4d2797f11abf183890ce32cf7
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-05-09 14:15:44 +0700

    Update bieudo.py

--- bieudo.py
+++ bieudo.py
@@ -11,7 +11,7 @@ def bieudo():
   tab1, tab2 = st.tabs(["Số lượng học sinh", "Điểm số"])
 
   with tab1:
-    nhanxet1 = np.array([['Giới tính','Gen','Nhìn chung học sinh Nam hứng thú với AI hơn học sinh Nữ.'],
+    nhanxet1 = np.array([['Giới tính','Gen','Nhìn chung học sinh Nam có hứng thú với AI hơn học sinh Nữ.'],
                ['Môn học chính khoá','Subject','Khối chuyên Toán quan tâm đến AI nhất và khối chuyên Trung - Nhật ít quan tâm nhất.'],
                ['Lớp Python','PYTHON-CLASS','Số học sinh ở 2 buổi và 2 phòng học gần bằng nhau, nên giờ học là hợp lý, đáp ứng được nhu cầu của tất cả học sinh.'],
                ['Khối lớp','Grade','Số lượng học sinh lớp 10 tham gia khoá PY4AI là nhiều nhất và số lượng học sinh lớp 12 là ít nhất.']])
