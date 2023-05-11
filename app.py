1d7d5a1ba46489f1fa4cd0f14852c5d498fd784a
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-05-11 23:47:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from phannhom import phannhom
 from phanloai import phanloai
 
 def main():
+  @st.cache
   st.title('BẢNG ĐIỂM LỚP PY4AI 09/2022')
   tab1, tab2, tab3, tab4 = st.tabs(['Danh sách','Biểu đồ','Phân nhóm','Phân loại'])
-  @st.cache
   
   with tab1:
     danhsach()
