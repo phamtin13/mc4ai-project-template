98b2798beb7641290352bd2039ae121ac697b455
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-05-11 23:51:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from bieudo import bieudo
 from phannhom import phannhom
 from phanloai import phanloai
 
-@st.cache
+@st.cache(suppress_st_warning=True)
 def main():
   st.title('BẢNG ĐIỂM LỚP PY4AI 09/2022')
   tab1, tab2, tab3, tab4 = st.tabs(['Danh sách','Biểu đồ','Phân nhóm','Phân loại'])
