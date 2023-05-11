e242eb91598a663d7641bfaaf67ec4d3d96be7b5
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-05-11 09:00:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from phanloai import phanloai
 def main():
   st.title('BẢNG ĐIỂM LỚP PY4AI 09/2022')
   tab1, tab2, tab3, tab4 = st.tabs(['Danh sách','Biểu đồ','Phân nhóm','Phân loại'])
+  st.caption('Made by Phạm Trương Hữu Tín.')
   
   with tab1:
     danhsach()
