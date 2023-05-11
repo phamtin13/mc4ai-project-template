d7c4e0182d0d6fead2d24137cec19d9dca38f223
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-05-12 06:58:46 +0700

    Update bieudo.py

--- bieudo.py
+++ bieudo.py
@@ -17,7 +17,7 @@ def bieudo():
                ['Môn học chính khoá','Subject','Khối chuyên Toán quan tâm đến AI nhất và khối chuyên Trung - Nhật ít quan tâm nhất.'],
                ['Lớp Python','PYTHON-CLASS','Số học sinh ở 2 buổi và 2 phòng học gần bằng nhau, nên giờ học là hợp lý, đáp ứng được nhu cầu của tất cả học sinh.'],
                ['Khối lớp','Grade','Số lượng học sinh lớp 10 tham gia khoá PY4AI là nhiều nhất và số lượng học sinh lớp 12 là ít nhất.'],
-               ['Tình trạng học','Status','Taylor'])
+               ['Tình trạng học','Status','Taylor']])
 
     pies = st.radio('Phân tích số lượng học sinh theo:', nhanxet1[:,0], horizontal=True)
     st.write(px.pie(dfmid, names = nhanxet1[:,1][nhanxet1[:,0]==pies][0]))
